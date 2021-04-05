2b9a7ed38b79b714de0228ebf5ec5fe64812c783
harrykim14 <harrykim14@korea.ac.kr>
2021-04-05 12:52:41 +0900

    Added responsive media query at project list page

--- pages/ProjectList.tsx
+++ pages/ProjectList.tsx
@@ -1,7 +1,7 @@
 import Image from 'next/image'
 import Layout from '../components/Layout'
 import GitHubIcon from '@material-ui/icons/GitHub'
-import LaunchIcon from '@material-ui/icons/Launch';
+import LaunchIcon from '@material-ui/icons/Launch'
 
 const ProjectList = () => {
   const discribeTextStyle =
@@ -20,35 +20,41 @@ const ProjectList = () => {
         </div>
         <div className={discribeTextStyle}>
           <h2 className={projectNameStyle}>
-            1. Project WeMo {' '}
+            1. Project WeMo{' '}
             <a
               className="hover:text-gray-400 cursor-pointer"
               href="https://github.com/harrykim14/ProjectWeMo"
               target="_blank"
             >
-              <GitHubIcon/>
+              <GitHubIcon />
             </a>
-            
           </h2>
-          <div className="flex flex-wrap flex-row justify-around my-6">
-            <Image
-              src="/projects/projectwemo1.jpg"
-              width={320}
-              height={180}
-              alt="project wemo login page"
-            />
-            <Image
-              src="/projects/projectwemo2.jpg"
-              width={480}
-              height={180}
-              alt="project wemo main page"
-            />
-            <Image
-              src="/projects/projectwemo3.jpg"
-              width={220}
-              height={180}
-              alt="project wemo chart page"
-            />
+          <div className="flex flex-wrap lg:flex-row md:flex-col justify-around my-6">
+            <div className="md:mb-4">
+              <Image
+                className="p-4"
+                src="/projects/projectwemo1.jpg"
+                width={320}
+                height={180}
+                alt="project wemo login page"
+              />
+            </div>
+            <div className="md:mb-4">
+              <Image
+                src="/projects/projectwemo2.jpg"
+                width={480}
+                height={180}
+                alt="project wemo main page"
+              />
+            </div>
+            <div className="md:mb-4">
+              <Image
+                src="/projects/projectwemo3.jpg"
+                width={220}
+                height={180}
+                alt="project wemo chart page"
+              />
+            </div>
           </div>
           <p className="mx-8">
             <span className="font-bold">Usage</span> : Java, Oracle DB, Spring
@@ -68,22 +74,25 @@ const ProjectList = () => {
 
         <div className={discribeTextStyle}>
           <h2 className={projectNameStyle}>
-            2. Seoul Bicycle Place {' '}
+            2. Seoul Bicycle Place{' '}
             <a
               className="hover:text-gray-400 cursor-pointer"
               href="https://github.com/harrykim14/seoul_bicycle_place"
               target="_blank"
             >
-              <GitHubIcon/>
+              <GitHubIcon />
             </a>
           </h2>
           <div className="flex flex-wrap flex-row justify-start my-6 mx-8">
-            <Image
-              src="/projects/seoulbycicle.jpg"
-              width={420}
-              height={180}
-              alt="seoul bycicle app page"
-            />
+            <div className="md:mb-4">
+              <Image
+                className="md:my-4"
+                src="/projects/seoulbycicle.jpg"
+                width={420}
+                height={180}
+                alt="seoul bycicle app page"
+              />
+            </div>
           </div>
           <p className="mx-8">
             <span className="font-bold">Usage</span> : React (mini project)
@@ -103,49 +112,60 @@ const ProjectList = () => {
 
         <div className={discribeTextStyle}>
           <h2 className={projectNameStyle}>
-            3. Wemo React Project {' '}
+            3. Wemo React Project{' '}
             <a
               className="hover:text-gray-400 cursor-pointer"
               href="https://github.com/harrykim14/wemo-react-project"
               target="_blank"
             >
-              <GitHubIcon/>
+              <GitHubIcon />
             </a>
           </h2>
           <div className="flex flex-wrap flex-row justify-around my-6">
-            <Image
-              src="/projects/wemoreactproject1.jpg"
-              width={320}
-              height={180}
-              alt="project wemo login page"
-            />
+            <div className="md:mb-4">
+              <Image
+                src="/projects/wemoreactproject1.jpg"
+                width={320}
+                height={180}
+                alt="project wemo login page"
+              />
+            </div>
             <Image
               src="/projects/wemoreactproject2.jpg"
               width={300}
               height={140}
               alt="project wemo main page"
             />
-            <Image
-              src="/projects/wemoreactproject3.jpg"
-              width={320}
-              height={180}
-              alt="project wemo setting page"
-            />
-             <Image
-              src="/projects/wemoreactproject4.jpg"
-              width={300}
-              height={140}
-              alt="project wemo logout page"
-            />
+            <div className="md:mb-4">
+              <Image
+                src="/projects/wemoreactproject3.jpg"
+                width={320}
+                height={180}
+                alt="project wemo setting page"
+              />
+            </div>
+            <div className="md:mb-4">
+              <Image
+                src="/projects/wemoreactproject4.jpg"
+                width={300}
+                height={140}
+                alt="project wemo logout page"
+              />
+            </div>
           </div>
           <p className="mx-8">
-            <span className="font-bold">Usage</span> : React Hook, Mongo DB, Node.js server, Material-ui
+            <span className="font-bold">Usage</span> : React Hook, Mongo DB,
+            Node.js server, Material-ui
             <br />
-            <span className="font-bold">Describe</span> : Rebuild the first memo app with React and mongo DB(modeling with Mongoose). Some UI/UX changed and improved user convenience. Also tried TDD method of development.
+            <span className="font-bold">Describe</span> : Rebuild the first memo
+            app with React and mongo DB(modeling with Mongoose). Some UI/UX
+            changed and improved user convenience. Also tried TDD method of
+            development.
             <br />
             <span className="font-bold">Duration</span> : 4 weeks
             <br />
-            <span className="font-bold">Participation</span> : 100% (personal project)
+            <span className="font-bold">Participation</span> : 100% (personal
+            project)
           </p>
         </div>
 
@@ -153,45 +173,51 @@ const ProjectList = () => {
 
         <div className={discribeTextStyle}>
           <h2 className={projectNameStyle}>
-            4. Firebase Chat app {' '}
+            4. Firebase Chat app{' '}
             <a
               className="hover:text-gray-400 cursor-pointer"
               href="https://github.com/harrykim14/bolierplate-class/tree/master/firebase-fourth-class"
               target="_blank"
             >
-              <GitHubIcon/>
-            </a>
-            {' '}
+              <GitHubIcon />
+            </a>{' '}
             <a
               className="hover:text-gray-400 cursor-pointer"
               href="https://react-chat-app-c511d.web.app/"
               target="_blank"
             >
-              <LaunchIcon/>
+              <LaunchIcon />
             </a>
           </h2>
           <div className="flex flex-wrap flex-row justify-around my-6">
-            <Image
-              src="/projects/firebasechatapp1.jpg"
-              width={260}
-              height={180}
-              alt="firebase chat app login page"
-            />
-            <Image
-              src="/projects/firebasechatapp2.jpg"
-              width={400}
-              height={140}
-              alt="firebase chat app main page"
-            />
+            <div className="md:mb-4">
+              <Image
+                src="/projects/firebasechatapp1.jpg"
+                width={260}
+                height={180}
+                alt="firebase chat app login page"
+              />
+            </div>
+            <div className="md:mb-4">
+              <Image
+                src="/projects/firebasechatapp2.jpg"
+                width={400}
+                height={140}
+                alt="firebase chat app main page"
+              />
+            </div>
           </div>
           <p className="mx-8">
-            <span className="font-bold">Usage</span> : React Hook, Node.js server, Google Firebase DB
+            <span className="font-bold">Usage</span> : React Hook, Node.js
+            server, Google Firebase DB
             <br />
-            <span className="font-bold">Describe</span> : A simple chat app with real-time database, Firebase. Front side was built by React Hook.
+            <span className="font-bold">Describe</span> : A simple chat app with
+            real-time database, Firebase. Front side was built by React Hook.
             <br />
             <span className="font-bold">Duration</span> : 3 weeks
             <br />
-            <span className="font-bold">Participation</span> : 100% (personal project)
+            <span className="font-bold">Participation</span> : 100% (personal
+            project)
           </p>
         </div>
 
@@ -199,51 +225,60 @@ const ProjectList = () => {
 
         <div className={discribeTextStyle}>
           <h2 className={projectNameStyle}>
-            5. NextJS blog with REST API using django REST framework {' '}
+            5. NextJS blog with REST API using django REST framework{' '}
             <a
               className="hover:text-gray-400 cursor-pointer"
               href="https://github.com/harrykim14/nextjs-blog-with-django-restapi"
               target="_blank"
             >
-              <GitHubIcon/>
-            </a>
-            {' '}
+              <GitHubIcon />
+            </a>{' '}
             <a
               className="hover:text-gray-400 cursor-pointer"
               href="https://nextjs-blog-with-django-restapi.vercel.app/"
               target="_blank"
             >
-              <LaunchIcon/>
+              <LaunchIcon />
             </a>
           </h2>
           <div className="flex flex-wrap flex-row justify-around my-6">
-            <Image
-              src="/projects/nextjsblog1.jpg"
-              width={200}
-              height={180}
-              alt="nextjs blog login page"
-            />
-            <Image
-              src="/projects/nextjsblog2.jpg"
-              width={380}
-              height={140}
-              alt="nextjs blog main page"
-            />
-            <Image
-              src="/projects/nextjsblog3.jpg"
-              width={380}
-              height={140}
-              alt="nextjs todo list page"
-            />
+            <div className="md:mb-4">
+              <Image
+                src="/projects/nextjsblog1.jpg"
+                width={200}
+                height={180}
+                alt="nextjs blog login page"
+              />
+            </div>
+            <div className="md:mb-4">
+              <Image
+                src="/projects/nextjsblog2.jpg"
+                width={380}
+                height={140}
+                alt="nextjs blog main page"
+              />
+            </div>
+            <div className="md:mb-4">
+              <Image
+                src="/projects/nextjsblog3.jpg"
+                width={380}
+                height={140}
+                alt="nextjs todo list page"
+              />
+            </div>
           </div>
           <p className="mx-8">
-            <span className="font-bold">Usage</span> : Python, django, django REST framework, Javascript, React, NextJs, Tailwind CSS
+            <span className="font-bold">Usage</span> : Python, django, django
+            REST framework, Javascript, React, NextJs, Tailwind CSS
             <br />
-            <span className="font-bold">Describe</span> : Simple blog page with NextJS, To-do list(CRUD) is changed with user event through django REST api modeled by django REST framework.
+            <span className="font-bold">Describe</span> : Simple blog page with
+            NextJS, To-do list(CRUD) is changed with user event through django
+            REST api modeled by django REST framework.
             <br />
             <span className="font-bold">Duration</span> : 1 weeks
             <br />
-            <span className="font-bold">Participation</span> : 100% (personal project)
+            <span className="font-bold">Participation</span> : 100% (personal
+            project)
           </p>
         </div>
 
@@ -251,39 +286,44 @@ const ProjectList = () => {
 
         <div className={discribeTextStyle}>
           <h2 className={projectNameStyle}>
-            6. GitHub Blog with Hexo framework {' '}
+            6. GitHub Blog with Hexo framework{' '}
             <a
               className="hover:text-gray-400 cursor-pointer"
               href="https://github.com/harrykim14/harrykim14.github.io"
               target="_blank"
             >
-              <GitHubIcon/>
-            </a>
-            {' '}
+              <GitHubIcon />
+            </a>{' '}
             <a
               className="hover:text-gray-400 cursor-pointer"
               href="https://harrykim14.github.io/"
               target="_blank"
             >
-              <LaunchIcon/>
+              <LaunchIcon />
             </a>
           </h2>
           <div className="flex flex-wrap flex-row justify-start my-6 mx-8">
-            <Image
-              src="/projects/hexoblog.jpg"
-              width={340}
-              height={180}
-              alt="GitHub blog page"
-            />
+            <div className="md:mb-4">
+              <Image
+                src="/projects/hexoblog.jpg"
+                width={340}
+                height={180}
+                alt="GitHub blog page"
+              />
+            </div>
           </div>
           <p className="mx-8">
-            <span className="font-bold">Usage</span> : Javascript based Hexo framework
+            <span className="font-bold">Usage</span> : Javascript based Hexo
+            framework
             <br />
-            <span className="font-bold">Describe</span> : Individual blog for summary personal studies that is built by Hexo framework.
+            <span className="font-bold">Describe</span> : Individual blog for
+            summary personal studies that is built by Hexo framework.
             <br />
-            <span className="font-bold">Duration</span> : 1 days to build, Keep updated
+            <span className="font-bold">Duration</span> : 1 days to build, Keep
+            updated
             <br />
-            <span className="font-bold">Participation</span> : 100% (personal project)
+            <span className="font-bold">Participation</span> : 100% (personal
+            project)
           </p>
         </div>
       </div>
